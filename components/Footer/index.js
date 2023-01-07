b4522a6cc6978925b86238ee6ba27303da7f37ba
Jack Riley <john_riley@brown.edu>
2023-01-07 10:56:51 -0800

    footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,7 +6,7 @@ import Button from "../Button";
 const Footer = ({}) => {
   return (
     <>
-      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
+      <div className="mt-5 laptop:mt-40 p-2 mb-5 laptop:p-0">
         <div className="">
           {/* <h1 className="text-2xl text-bold">Contact</h1> */}
           <div className="mt-10" >
@@ -15,19 +15,15 @@ const Footer = ({}) => {
             </h1> */}
 
             <div style={{"display":"flex"}}>
-            <Socials className="mt-5"/>
+            <Socials className="mt-5 mb-24 mr-5"/>
             <p className="mt-4 ml-2">jackbriley@me.com</p>
             </div>
 
           </div>
         </div>
       </div>
-      <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        {/* Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
-        </Link> */}
-      </h1>
+      {/* <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
+      </h1> */}
     </>
   );
 };
